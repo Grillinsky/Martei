6bea083995494a1dcf10f6531f7baaa2ec7a62e7
Martin Grillo <martine.grillo@gmail.com>
2023-06-14 15:12:36 -0300

    una humilde foto que me saque y +

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function ProductCard() {
+  return (
+    <div>
+      {/* 
+        img
+        name
+        price
+        description
+        buy button
+        add to cart button
+        
+    
+    */}
+    </div>
+  );
+}
+
+export default ProductCard;
---
+++ src/pages/Products.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Product() {
+  return (
+    <div>
+      {/* for (product of products){
+      <ProductCard key={product.id} products={products}
+    } */}
+    </div>
+  );
+}
+
+export default Product;
