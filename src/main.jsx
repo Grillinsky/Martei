aacb790d8253e697b7a5da6fd7d58e01fe2cd91e
Martin Grillo <martine.grillo@gmail.com>
2023-06-20 13:37:15 -0300

    Merge branch 'main' of https://github.com/Grillinsky/Martei

--- src/main.jsx
+++ src/main.jsx
@@ -3,18 +3,18 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-/* import { Provider } from "react-redux";
+import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
-import { store, persistor } from "../redux/storeConfig.js"; */
+import { store, persistor } from "../redux/storeConfig.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* <Provider store={store}> */}
-      {/* <PersistGate loading={null} persistor={persistor}> */}
+     <Provider store={store}>
+       <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter>
           <App />
         </BrowserRouter>
-    {/*   </PersistGate>
-    </Provider> */}
+      </PersistGate>
+    </Provider> 
   </React.StrictMode>
 );
