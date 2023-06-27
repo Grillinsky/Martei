2b7393be8465ba8052af0223673172c5bb4d5a51
AlexTGarcia020 <alextgarcia020@gmail.com>
2023-06-27 12:38:34 -0300

    OffCanvas About Proyect V1

--- src/components/OffCanvas.jsx
+++ src/components/OffCanvas.jsx
@@ -1,12 +1,23 @@
 import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Offcanvas from "react-bootstrap/Offcanvas";
+import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import "../css/OffCanvas.css"
 
 function OffCanvas() {
   const [show, setShow] = useState(false);
+  const { setValue } = useForm();
+
+const handleLogin = () => {
+  setValue("username", "pabloperez@gmail.com");
+  setValue("password", "123");
+};
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+
   return (
     <>
       <Button id="offCanvasButton" onClick={handleShow}>
@@ -14,20 +25,54 @@ function OffCanvas() {
       </Button>
 
       <Offcanvas show={show} onHide={handleClose} placement="end">
-        <Offcanvas.Header closeButton>
-          <Offcanvas.Title>Proyecto final Martei.</Offcanvas.Title>
+        <Offcanvas.Header className="off-canvas-header">
+          <Offcanvas.Title className="title-canvas">Proyecto final Martei.</Offcanvas.Title>
+        <i className="fa fa-window-close" onClick={handleClose} aria-hidden="true"></i>
         </Offcanvas.Header>
         <Offcanvas.Body>
-          <div className="border container">
-            <dd>
-              Bienvenid@ al proyecto, desde aquí tendrás 2 opciones, ingresar
-              como User:
+        <div className="container container-off-canvas">
+            <h1 className="fs-2">Sobre Este Proyecto:</h1>
+            <dd className="fs-6">
+            Martei es un e-commerce creado como proyecto final del Bootcamp de HackAcademy Fue desarrolado durante 3 semanas por 4 estudiantes. Haz click para más información!
             </dd>
-            <ul>
-              <li>User: pabloperez@gmail.com</li>
-              <li>Password: 123</li>
+          </div>
+          <div className="off-canvas-button-div">
+          <button as={Link} to={"/login"} className="btn-user-canvas" onClick={handleLogin}>
+            Sobre Nosotros
+          </button>
+          </div>
+          <hr className="m-2"/>
+          <div className="container container-off-canvas">
+            <h1 className="fs-2">Guia De Navegacion:</h1>
+            <dd className="fs-6">
+            Sientete libre de navegar por este sitio, agrega productos a tu carrito, y finaliza tu compra logeandote con las credenciales de usuario:
+            </dd>
+            <ul className="list-off-canvas">
+              <li>Usuario: User@User.com</li>
+              <li>Contraseña: 123</li>
             </ul>
           </div>
+          <div className="off-canvas-button-div">
+          <button as={Link} to={"/login"} className="btn-user-canvas" onClick={handleLogin}>
+            Login User
+          </button>
+          </div>
+          <hr className="m-2"/>
+          <div className="container container-off-canvas">
+            <dd className="fs-6">
+            Si quieres probar las funcionalidades de Admin, como crear, aditar o borrar ya sea categorias, productos, usuarios u ordenes puedes acceder con las siguientes credenciales:
+            </dd>
+            <ul className="list-off-canvas">
+              <li>Usuario: Admin@Admin.com</li>
+              <li>Contraseña: 123</li>
+            </ul>
+          </div>
+          <div className="off-canvas-button-div">
+          <button as={Link} to={"/adminLogin"} className="btn-user-canvas" onClick={handleLogin}>
+            Login Admin
+          </button>
+          </div>
+          <hr id="hr-canvas"/>
         </Offcanvas.Body>
       </Offcanvas>
     </>
