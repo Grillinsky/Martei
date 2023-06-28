9d2611518835d86f22b95946380408937618bebb
AlexTGarcia020 <alextgarcia020@gmail.com>
2023-06-28 17:40:56 -0300

    footer final v2

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,11 +8,11 @@ function Footer() {
       <div className="row" id="footerContainer">
         <div className="col-lg-6">
           <div className="row">
-            <div className="col-4">
+            <div className="col-4" style={{marginTop:"40px"}}>
               <img src="/logoFinal.png" alt="Logo Martei" />
             </div>
-            <div className="col-4">
-              <dd className="footer-name ms-4">Catálogo</dd>
+            <div className="col-4" style={{marginTop:"5px"}}>
+              <dd className="footer-name" style={{marginLeft:"31px"}}>Catálogo</dd>
               <ul>
                 <li>
                   <a href="/category/list/1">Muebles</a>
@@ -31,8 +31,8 @@ function Footer() {
                 </li>
               </ul>
             </div>
-            <div className="col-4">
-              <dd className="footer-name ms-4">Sobre Martei</dd>
+            <div className="col-4" style={{marginTop:"30px"}}>
+              <dd className="footer-name" style={{marginLeft:"31px"}}>Sobre Martei</dd>
               <ul>
                 <Link to={"/aboutUs"}>
                 <li>Nosotros</li>
@@ -51,25 +51,26 @@ function Footer() {
                 <MyMap />
               </div>
             </div>
-            <div className="col-6 col-md-6">
-              <ul>
-                <li className="footer-name">
-                  <Link to={"https://goo.gl/maps/ELcTZ1L7QwGNi1Lm9"}>
-                    <h4 className=""> Ciudad Vieja</h4>
-                  </Link>
-                </li>
-                <li>Montevideo</li>
-                <li>099470542</li>
-                <li className="p-3">
-                  <a
-                    aria-label="Link to Whatsapp"
-                    href="https://wa.link/n6nt6k"
-                  >
-                    <i className="fab fa-whatsapp"></i>
-                  </a>
-                </li>
-              </ul>
-            </div>
+            <div className="col-6 col-md-6 mt-4">
+  <ul>
+    <li className="footer-name" style={{ display: "flex", alignItems: "center" }}>
+      <i className="fas fa-map-pin fs-5" style={{ marginRight: "1px" }}></i>
+      <h4 className="fs-6 mt-2 ms-3">Ciudad Vieja, Montevideo</h4>
+    </li>
+    <li className="footer-name mt-2" style={{ display: "flex", alignItems: "center" }}>
+      <i className="fas fa-phone-alt fs-6" style={{ marginRight: "8px" }}></i>
+      <span className="fw-normal ms-1">099470542</span>
+    </li>
+    <li className="mt-3">
+      <a
+        aria-label="Chatea con nosotros en WhatsApp"
+        href="https://wa.link/n6nt6k"
+      >
+       <i className="fab fa-whatsapp fs-5 ms-0 mx-1 fw-normal"></i><span className="ms-2">Chatea con nosotros</span>
+      </a>
+    </li>
+  </ul>
+</div>
           </div>
         </div>
       </div>
